166282d0a10521b38714fa9cbf7d2255b9f8f2cd
Jagroop Singh <jagroopofficial2000@gmail.com>
2022-08-13 18:01:06 +0530

    Learn Logical Operator and Short Circuiting in Python

---
+++ logical_operators.py
@@ -0,0 +1,36 @@
+# > , < , == ,>=,<= , != , and , or , not 
+print(1 >3)
+print(1 <3)
+print(1 == 1)
+print(1 >= 0)
+print(3 <=3)
+print(1 != 3)
+print(1 >3 and 1<2)
+print(1 <3 or 1<2)
+print(not(1==1))
+print(not True)
+
+# Exercise :
+
+is_magician = False
+is_expert = True
+
+if is_magician and is_expert :
+    print("You are a master magicial")
+
+elif is_magician and not is_expert:
+    print("Atleast you are getting there")
+else :
+    print("You are not a magician")
+
+
+
+# is Operator
+
+print("1" is "1")
+print("1" is 1)
+print(1 == 1.0)
+print([1,2,3] is [1,2,3])
+
+
+
---
+++ short_circuiting.py
@@ -0,0 +1,12 @@
+#Short Circuiting
+
+is_friend = True
+is_friend = True
+
+# if True or True 
+# Above it firstly check if Fistly it Encountered True then it doesnot check further
+
+# if False and True 
+# Above it firstly check if Fistly it Encountered False then it doesnot check further  
+
+# This is known as short Circuiting
