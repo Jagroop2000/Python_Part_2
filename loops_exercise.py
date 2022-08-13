8fe92c5166b86d20c06b3b7d6f15c2a7c932d947
Jagroop Singh <jagroopofficial2000@gmail.com>
2022-08-13 18:23:50 +0530

    For Loop , Range

---
+++ loops_exercise.py
@@ -0,0 +1,21 @@
+my_list =[1,2,3,4,5,6,7,8,9,10]
+
+sum =0
+
+for items in my_list :
+    sum+=items
+
+print(sum)
+
+
+counter =0
+for  _ in range(1,100,2):
+    counter+=_
+
+print(counter)
+
+counter =0
+for  _ in range(100,1,-1):
+    counter+=_
+
+print(counter)
